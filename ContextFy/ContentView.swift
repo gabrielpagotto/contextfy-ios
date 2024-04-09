8c2e3673e55707d974eedf0fa87c761eaeeffe67
Gabriel Pagotto <gabriel.batista@tradexsolutions.com.br>
2024-04-09 14:09:58 -0300

    Initial Commit

---
+++ ContextFy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContextFy
+//
+//  Created by Gabriel Pagotto on 09/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ContextFy/ContextFyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextFyApp.swift
+//  ContextFy
+//
+//  Created by Gabriel Pagotto on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ContextFyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
