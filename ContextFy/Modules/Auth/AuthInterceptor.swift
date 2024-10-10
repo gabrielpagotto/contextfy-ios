103e27c12739b08d9b1d009ac43922df072b1393
Gabriel Pagotto <gabriel.batista@tradexsolutions.com.br>
2024-10-10 17:40:48 -0300

    Add contextfy api client

---
+++ ContextFy/Modules/Auth/AuthInterceptor.swift
@@ -0,0 +1,35 @@
+//
+//  AuthInterceptor.swift
+//  ContextFy
+//
+//  Created by Gabriel Pagotto on 10/10/24.
+//
+
+import Foundation
+import Alamofire
+
+class AuthInterceptor : RequestInterceptor {
+    
+    private func removeAccessToken() {
+        UserDefaults.standard.removeObject(forKey: "contextfy.access_token")
+    }
+    
+    private func getAccessToken() -> String? {
+        return UserDefaults.standard.string(forKey: "contextfy.access_token")
+    }
+    
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        var urlRequest = urlRequest
+        if let accessToken = getAccessToken() {
+            urlRequest.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        }
+        completion(.success(urlRequest))
+    }
+    
+    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+        if let response = request.task?.response as? HTTPURLResponse {
+            if response.statusCode == 401 { removeAccessToken() }
+        }
+        completion(.doNotRetry)
+    }
+}
